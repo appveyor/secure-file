1d2daab929d76add21d1c2cfcd09ccc770fcff4f
Feodor Fitsner <feodor@appveyor.com>
2018-04-03 16:49:01 -0700

    Forgot chmod

--- install.ps1
+++ install.ps1
@@ -5,4 +5,7 @@ if ($isLinux) {
 $zipPath = Join-Path $temp 'secure-file.zip'
 (New-Object Net.WebClient).DownloadFile('https://github.com/appveyor/secure-file/releases/download/1.0.0/secure-file.zip', $zipPath)
 Expand-Archive $zipPath -DestinationPath (Join-Path (pwd).path "appveyor-tools")
+if ($isLinux) {
+	chmod +x ./appveyor-tools/secure-file
+}
 del $zipPath
