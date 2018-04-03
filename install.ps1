fc2e6e7d95ade9a40425c8c3bf509baab6c20d37
Feodor Fitsner <feodor@appveyor.com>
2018-04-03 16:14:15 -0700

    install.ps1 ready

--- install.ps1
+++ install.ps1
@@ -1,1 +1,4 @@
-Write-Host "Installing secure-file utility..."
+$zipPath = "$env:temp\secure-file.zip"
+(New-Object Net.WebClient).DownloadFile('https://github.com/appveyor/secure-file/releases/download/1.0.0/secure-file.zip', $zipPath)
+Expand-Archive $zipPath -DestinationPath (Join-Path (pwd).path "appveyor-tools")
+del $zipPath
