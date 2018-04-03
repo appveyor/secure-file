0d35e5b4c4f600a23cefef68ea10003905f42b76
Feodor Fitsner <feodor@appveyor.com>
2018-04-03 16:21:29 -0700

    install.sh ready

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-Echo "Installing secure-file utility"
+TMP_DIR=$(mktemp -d)
+curl -fsSL -O https://github.com/appveyor/secure-file/releases/download/1.0.0/secure-file.zip
+unzip -q -o secure-file.zip -d appveyor-tools
+chmod +x ./appveyor-tools/secure-file
+rm secure-file.zip
