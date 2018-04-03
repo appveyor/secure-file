2e8d6082141ffdfe21e234bb8bb7949c4ecdc61f
Feodor Fitsner <feodor@appveyor.com>
2018-04-03 16:29:37 -0700

    do not create temp folder

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-TMP_DIR=$(mktemp -d)
 curl -fsSL -O https://github.com/appveyor/secure-file/releases/download/1.0.0/secure-file.zip
 unzip -q -o secure-file.zip -d appveyor-tools
 chmod +x ./appveyor-tools/secure-file
