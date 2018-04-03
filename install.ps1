6e474f73b8926f87ee3566438cc5ea19ef849768
Feodor Fitsner <feodor@appveyor.com>
2018-04-03 16:48:14 -0700

    PS installation script is platform-agnostic

--- install.ps1
+++ install.ps1
@@ -1,4 +1,8 @@
-$zipPath = "$env:temp\secure-file.zip"
+$temp = $env:temp
+if ($isLinux) {
+	$temp = '/tmp'
+}
+$zipPath = Join-Path $temp 'secure-file.zip'
 (New-Object Net.WebClient).DownloadFile('https://github.com/appveyor/secure-file/releases/download/1.0.0/secure-file.zip', $zipPath)
 Expand-Archive $zipPath -DestinationPath (Join-Path (pwd).path "appveyor-tools")
 del $zipPath
