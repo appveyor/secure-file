4c320bb6137c5664124b3e102d3d5bcfa3a8c7b3
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-07-18 22:18:31 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ New-Item -ItemType Directory -Path $tempdir
 
 $zipPath = Join-Path $tempdir 'secure-file.zip'
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-(New-Object Net.WebClient).DownloadFile('https://github.com/jeff-winn/secure-file/releases/download/1.1.0/secure-file.zip', $zipPath)
+(New-Object Net.WebClient).DownloadFile('https://github.com/jeff-winn/secure-file/releases/download/1.1.1/secure-file.zip', $zipPath)
 Expand-Archive $zipPath -DestinationPath (Join-Path (pwd).path "appveyor-tools") -Force
 if ($isLinux) {
 	chmod +x ./appveyor-tools/secure-file
