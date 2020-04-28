42fe0266fbd49d6103758bb6efd74849f1946b7f
Feodor Fitsner <feodor@appveyor.com>
2020-04-28 15:01:58 -0700

    Correct path to build folder

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 rd /s /q build
-dotnet publish secure-file -f netcoreapp3.1 -o ..\build -c Release
-dotnet publish secure-file -f net462 -o ..\build -c Release
+dotnet publish secure-file -f netcoreapp3.1 -o build -c Release
+dotnet publish secure-file -f net462 -o build -c Release
