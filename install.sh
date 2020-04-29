5b97b5838a3e8b19268ecfaf210eb7aa6b509ca2
Feodor Fitsner <feodor@appveyor.com>
2020-04-29 10:56:18 -0700

    secure-file 1.0.1

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-curl -fsSL -O https://github.com/appveyor/secure-file/releases/download/1.0.0/secure-file.zip
+curl -fsSL -O https://github.com/appveyor/secure-file/releases/download/1.0.1/secure-file.zip
 unzip -q -o secure-file.zip -d appveyor-tools
 chmod +x ./appveyor-tools/secure-file
 rm secure-file.zip
