fc44f5c5f2fb184fe738814f373c16cb69c30929
Feodor Fitsner <feodor@appveyor.com>
2020-07-15 12:21:09 -0700

    Additional check for macOS

--- install.ps1
+++ install.ps1
@@ -7,7 +7,7 @@ $zipPath = Join-Path $tempdir 'secure-file.zip'
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 (New-Object Net.WebClient).DownloadFile('https://github.com/appveyor/secure-file/releases/download/1.0.1/secure-file.zip', $zipPath)
 Expand-Archive $zipPath -DestinationPath (Join-Path (pwd).path "appveyor-tools") -Force
-if ($isLinux) {
+if ($isLinux -or $isMacOS) {
 	chmod +x ./appveyor-tools/secure-file
 }
 del $tempdir -Recurse -Force
